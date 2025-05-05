65f03e66c462e32277443a828c7fccc4722b0285
Edilson Botto <edbotto@edu.unifor.br>
2025-05-05 20:40:20 -0300

    Update desafio.py
    Melhorias no extrato.

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -55,7 +55,10 @@ while True:
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
+        if not extrato:
+            print("Não foram realizadas movimentações.")
+        else:
+            print(extrato, end="")
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
